62f7a18d170c7844501328bd21a39cba954cbac1
KikoIshimoto <ishimoto.life@gmail.com>
2021-09-10 01:13:32 +0900

    delete log

--- src/Dungeon/DungeonCreate.cpp
+++ src/Dungeon/DungeonCreate.cpp
@@ -80,7 +80,6 @@ void DungeonCreate::buildStart(){
     int y_ = y+1;
     int width = 2 + mt()%3;
     int height = 2 + mt()%3;
-    std::cout << "room create" << std::endl;
     for(int xx = x_ ; xx < x_+width && xx < width_ ; xx++){
       for(int yy = y_ ; yy < y_+height && yy < height_; yy++){
         bitmap[yy][xx] = NONE;
